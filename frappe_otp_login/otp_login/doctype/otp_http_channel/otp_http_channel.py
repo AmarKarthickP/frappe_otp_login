dc7c4b92aee7898806340a64e5f31067dfea557b
Sumanta Gogoi <sumanta@xynocast.com>
2026-05-09 09:51:16 +0300

    feat: add URL validation and preset HTTP channels (ntfy, Indian SMS)

--- frappe_otp_login/otp_login/doctype/otp_http_channel/otp_http_channel.py
+++ frappe_otp_login/otp_login/doctype/otp_http_channel/otp_http_channel.py
@@ -1,5 +1,19 @@
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class OTPHTTPChannel(Document):
-	pass
+	def validate(self):
+		self.validate_url()
+
+	def validate_url(self):
+		if not self.url:
+			return
+		url = self.url.strip()
+		if not url.startswith(("http://", "https://")):
+			frappe.throw(_("URL must start with http:// or https://"))
+		if len(url) < 10:
+			frappe.throw(_("URL is too short to be valid"))
+		if " " in url:
+			frappe.throw(_("URL must not contain spaces"))
